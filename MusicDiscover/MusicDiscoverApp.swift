386f2e08e0b3ba5c90cd91ff3da0ca5934204c27
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2025-12-29 21:51:05 +0300

    Initial SwiftUI project setup for Music Discover

---
+++ MusicDiscover/MusicDiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicDiscoverApp.swift
+//  MusicDiscover
+//
+//  Created by Sedef Bozkurt on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicDiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
