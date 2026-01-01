f7d183365298a6e561defae20808ecbf1d94e47c
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2026-01-01 20:14:48 +0300

    Add reusable section header component

---
+++ MusicDiscover/Components/SectionHeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  SectionHeaderView.swift
+//  MusicDiscover
+//
+//  Created by Sedef Bozkurt on 1.01.2026.
+//
+
+import SwiftUI
+
+struct SectionHeaderView: View {
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.title3)
+                .fontWeight(.semibold)
+                
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    SectionHeaderView(title: "Top Picks for You")
+}
