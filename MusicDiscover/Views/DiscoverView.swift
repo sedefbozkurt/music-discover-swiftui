6c12566b9a75f3e73663feca18570dd3fe29ca3e
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2026-01-05 18:30:59 +0300

    Add DiscoverView skeleton

---
+++ MusicDiscover/Views/DiscoverView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverView.swift
+//  MusicDiscover
+//
+//  Created by Sedef Bozkurt on 5.01.2026.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    var body: some View {
+        Text("Discover")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+    }
+}
+
+#Preview {
+    DiscoverView()
+}
